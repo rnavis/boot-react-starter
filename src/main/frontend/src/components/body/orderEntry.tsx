aaba043accdd71bdaf56c2f28ca9b7c8a1584d91
sivanandanr <ranjith.sivanandan@redi.com>
2018-04-27 16:05:45 -0400

    sample hook ui to server

--- src/main/frontend/src/components/body/orderEntry.tsx
+++ src/main/frontend/src/components/body/orderEntry.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Side from './side';
+import 'whatwg-fetch';
 interface OrderEntryProps {
     side: string;
     changeTitle: (string) => void;
@@ -11,6 +12,13 @@ export default class OrderEntry extends React.Component<OrderEntryProps, {}> {
     }
     addChange(e) {
         this.props.changeTitle(e.target.value);
+        fetch(`/echo?request=${e.target.value}`)
+            .then(response => {
+                return response.text();
+            })
+            .then(body => {
+                alert(body);
+            });
     }
     render() {
         return (
